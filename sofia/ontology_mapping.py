8badf244a2beec2f056585aa1609860225139c0a
Michael Reynolds <michael.reynolds@twosixlabs.com>
2022-03-03 11:26:59 -0500

    tweaks

--- sofia/ontology_mapping.py
+++ sofia/ontology_mapping.py
@@ -103,7 +103,11 @@ class Ontology:
         return "", ""
 
     def format_ontology(self, ontology_name, save_file):
-        file_name = f'/data/{ontology_name}.yml'
+        if isabs(ontology_name):
+            file_name = ontology_name
+        else:
+            file_name = f'/data/{ontology_name}.yml'
+
         # file_path = os.path.dirname(os.path.abspath(file_name))+file_name
         file = os.path.dirname(os.path.abspath(__file__)) + file_name
         with open(file) as f:
